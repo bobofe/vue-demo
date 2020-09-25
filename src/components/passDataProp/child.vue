d78a9cbee9f539bd129ca1483eec139a0d5493d4
bobofe <836050837@qq.com>
2020-09-25 15:39:18 +0800

    first commit

---
+++ src/components/passDataProp/child.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <div>这是子组件</div>
+    <div>{{ this.message }}</div>
+    <div>{{ this.arrData }}</div>
+    <div>{{ this.username }}</div>
+  </div>
+</template>
+
+<script type='text/ecmascript-6'>
+export default {
+  props: ['message', 'arrData', 'username'],
+  mounted () {
+  },
+  data () {
+    return {
+      age: null
+    }
+  }
+}
+</script>
---
+++ src/components/passDataProp/parent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id='app'>
+    <div>这是父组件</div>
+    <child :message='message' :arrData = 'arrData' :username = 'username'></child>
+  </div>
+</template>
+
+<script>
+import child from './child'
+export default {
+  name: 'app',
+  components: { child },
+  data () {
+    return {
+      message: {
+        age: ''
+      },
+      arrData: null,
+      username: ''
+    }
+  },
+  mounted () {
+    // 模拟异步请求
+    setTimeout(() => {
+      console.error('我执行了')
+      this.message.age = 18
+      this.arrData = [
+        {
+          'id': 1
+        }
+      ]
+      this.username = 'lili'
+    }, 2000)
+  }
+}
+</script>
